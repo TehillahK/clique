9f1586a5a73f5834618f04f29c1c6f396a7d58a6
Tehillah Kangamba <>
2021-10-17 20:32:34 -0500

    added css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
-import './stylesheets/scss/main.scss'
+import './stylesheets/dist/css/main.css'
 ReactDOM.render(
   <React.StrictMode>
     <App />
